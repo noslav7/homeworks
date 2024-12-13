5465bbd772591952e56231a1ddc576546aeff7d3
noslav7 <noslav7@gmail.com>
2024-12-13 11:50:26 +0500

    HW-2 Kafka fix - modified: application.properties KafkaConsumerService KafkaProducerService TaskService - created: KafkaConfig KafkaMessageDTO

--- src/main/java/com/example/aspect_oriented_programming/kafka/KafkaProducerService.java
+++ src/main/java/com/example/aspect_oriented_programming/kafka/KafkaProducerService.java
@@ -1,17 +1,30 @@
 package com.example.aspect_oriented_programming.kafka;
 
+import com.example.aspect_oriented_programming.dto.KafkaMessageDTO;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 
 @Service
 public class KafkaProducerService {
+    private final Logger logger = LoggerFactory.getLogger(KafkaProducerService.class);
     private final KafkaTemplate<String, String> kafkaTemplate;
+    private final ObjectMapper objectMapper;
 
-    public KafkaProducerService(KafkaTemplate<String, String> kafkaTemplate) {
+    public KafkaProducerService(KafkaTemplate<String, String> kafkaTemplate, ObjectMapper objectMapper) {
         this.kafkaTemplate = kafkaTemplate;
+        this.objectMapper = objectMapper;
     }
 
-    public void sendMessage(String topic, String message) {
-        kafkaTemplate.send(topic, message);
+    public void sendMessage(String topic, KafkaMessageDTO messageDTO) {
+        try {
+            String message = objectMapper.writeValueAsString(messageDTO);
+            logger.info("Отправлено сообщение: {}", message);
+            kafkaTemplate.send(topic, message);
+        } catch (Exception e) {
+            logger.error("Ошибка сериализации сообщения: ", e);
+        }
     }
 }
